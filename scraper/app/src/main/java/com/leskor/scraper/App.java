117700789ce113dd90839b43609acf4763bb4907
Ruslan Lesko <ruslanlesko@gmail.com>
2021-11-13 12:16:46 +0200

    Implement Kafka posts producer

--- scraper/app/src/main/java/com/leskor/scraper/App.java
+++ scraper/app/src/main/java/com/leskor/scraper/App.java
@@ -14,25 +14,28 @@ import java.util.concurrent.Executors;
 
 public class App {
     private static final Logger logger = LoggerFactory.getLogger("Application");
-    public static final String VERSION = "0.0.2";
+    public static final String VERSION = "0.1.0";
 
     public static void main(String[] args) {
         logger.info("Starting Scraper {}", VERSION);
 
         ExecutorService pool = Executors.newFixedThreadPool(16);
+        KafkaPostsProducer kafkaPostsProducer = new KafkaPostsProducer();
 
         var rssFactory = new RSSFactory(new ObjectMapper(), "rss.json", createHttpClient(pool));
         try {
             var rssSites = rssFactory.createSites();
             for (var site : rssSites) {
-                site.fetchPosts()
-                        .join()
-                        .forEach(p -> logger.debug("{} -> {}", p.publicationTime(), p.title()));
+                var posts = site.fetchPosts().join();
+                logger.debug("Sending posts:");
+                posts.forEach(p -> logger.debug("{} -> {}", p.publicationTime(), p.title()));
+                kafkaPostsProducer.sendPosts(posts);
             }
         } catch (IOException e) {
             logger.error("Failed to create RSS sites", e);
         }
 
+        kafkaPostsProducer.close();
         pool.shutdown();
     }
 
---
+++ scraper/app/src/main/java/com/leskor/scraper/KafkaPostsProducer.java
@@ -0,0 +1,67 @@
+package com.leskor.scraper;
+
+import com.leskor.scraper.entities.Post;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.System.getenv;
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
+
+public class KafkaPostsProducer {
+    private static final Logger logger = LoggerFactory.getLogger("Application");
+
+    private static final String KAFKA_ADDRESS = getenv("SCR_KAFKA_ADDRESS") == null ?
+            "localhost:9092" : getenv("SCR_KAFKA_ADDRESS");
+    private static final String TOPIC = "posts";
+
+    private final Producer<String, String> producer;
+
+    public KafkaPostsProducer() {
+        Properties props = new Properties();
+        props.put(BOOTSTRAP_SERVERS_CONFIG, KAFKA_ADDRESS);
+        props.put(ACKS_CONFIG, "all");
+        props.put(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        this.producer = new KafkaProducer<>(props);
+    }
+
+    public void sendPosts(List<Post> posts) {
+        final var countDownLatch = new CountDownLatch(posts.size());
+
+        try {
+            for (Post post : posts) {
+                final ProducerRecord<String, String> record =
+                        new ProducerRecord<>(TOPIC, post.title());
+                producer.send(record, (metadata, exception) -> {
+                    if (metadata != null) {
+                        logger.debug("sent record(key={} value={}) meta(partition={}, offset={})",
+                                record.key(), record.value(), metadata.partition(), metadata.offset());
+                    } else {
+                        logger.error("Failed to send post", exception);
+                    }
+                    countDownLatch.countDown();
+                });
+            }
+            if (!countDownLatch.await(25, TimeUnit.SECONDS)) {
+                logger.error("Sending posts timed out");
+            }
+        } catch (InterruptedException e) {
+            logger.error("Interrupted", e);
+        } finally {
+            producer.flush();
+        }
+    }
+
+    public void close() {
+        producer.close();
+    }
+}
