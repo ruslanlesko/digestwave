97e3d9dc790db1a368662a87a350d35af471f905
Ruslan Lesko <ruslanlesko@gmail.com>
2022-04-18 17:10:22 +0300

    Setup CORS for articles controller

--- provider/app/src/main/java/com/leskor/provider/web/ArticlesController.java
+++ provider/app/src/main/java/com/leskor/provider/web/ArticlesController.java
@@ -5,6 +5,7 @@ import com.leskor.provider.entities.ArticlePreview;
 import com.leskor.provider.exceptions.NotFoundException;
 import com.leskor.provider.services.ArticlesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@CrossOrigin(origins = "http://localhost:8091", maxAge = 3600)
 @RestController
 public class ArticlesController {
     private final ArticlesService articlesService;
