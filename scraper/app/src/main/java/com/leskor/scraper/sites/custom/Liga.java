c284e1726d9c82612c2505ac04ef167ab43e25e3
Ruslan Lesko <ruslanlesko@gmail.com>
2022-08-11 18:59:49 +0300

    Implement custom site fetching for liga.net

---
+++ scraper/app/src/main/java/com/leskor/scraper/sites/custom/Liga.java
@@ -0,0 +1,145 @@
+package com.leskor.scraper.sites.custom;
+
+import com.leskor.scraper.dto.ReadabilityResponse;
+import com.leskor.scraper.entities.Post;
+import com.leskor.scraper.entities.Region;
+import com.leskor.scraper.entities.Topic;
+import com.leskor.scraper.sites.Site;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+import static java.time.Duration.ofSeconds;
+
+public class Liga extends Site {
+    private static final Logger logger = LoggerFactory.getLogger("Application");
+
+    private static final URI INDEX_URI = URI.create("https://finance.liga.net/ua");
+    private static final int POSTS_LIMIT = 10;
+
+    public Liga(URI readabilityUri, HttpClient httpClient) {
+        super(INDEX_URI, readabilityUri, "LIG", httpClient, ofSeconds(60), Topic.FINANCE, Region.UA, Set.of());
+    }
+
+    @Override
+    protected List<CompletableFuture<Post>> extractPostsBasedOnPage(String page) {
+        Document document = Jsoup.parse(page, Parser.htmlParser());
+        Element newsElem = document.getElementById("news-list-right");
+        if (newsElem == null) {
+            logger.warn("Failed to locate news on Liga.net");
+            return List.of();
+        }
+
+        return newsElem.getElementsByTag("li").stream()
+                .filter(e -> e.getElementsByClass("other-project").isEmpty())
+                .filter(parentElem -> {
+                    Element e = parentElem.getElementsByTag("a").first();
+                    return e != null && e.hasAttr("href") && e.hasText();
+                })
+                .limit(POSTS_LIMIT)
+                .map(parentElem -> {
+                    Element e = parentElem.getElementsByTag("a").first();
+                    return extractPost(
+                            e.text(),
+                            URI.create(e.attr("href")),
+                            parseTime(parentElem.getElementsByClass("time").first())
+                    );
+                })
+                .toList();
+    }
+
+    private CompletableFuture<Post> extractPost(String title, URI uri, ZonedDateTime time) {
+        if (time == null) return CompletableFuture.completedFuture(null);
+
+        CompletableFuture<ReadabilityResponse> readabilityFuture = retrieveReadabilityResponse(uri)
+                .thenApply(r -> r == null ? null : ReadabilityResponse.fromTitleAndExistingResponse(title, r));
+
+        return readabilityFuture.thenCombine(extractImageURL(uri), (readabilityResponse, imageUriString) -> {
+            if (readabilityResponse == null) {
+                return null;
+            }
+            URI imageURI = imageUriString.map(URI::create).orElse(null);
+            return Post.from(siteCode, topic, region, time, readabilityResponse, readabilityResponse.title(), uri, imageURI);
+        });
+    }
+
+    private ZonedDateTime parseTime(Element timeElem) {
+        if (timeElem == null || !timeElem.hasText()) return null;
+
+        String raw = timeElem.text();
+        String yesterdayPrefix = "вчора о ";
+        boolean isYesterday = false;
+        if (raw.startsWith(yesterdayPrefix)) {
+            raw = raw.substring(yesterdayPrefix.length());
+            isYesterday = true;
+        }
+
+        if (raw.length() != 5) {
+            return null;
+        }
+
+        int hour = Integer.parseInt(raw.substring(0, 2), 10);
+        int minute = Integer.parseInt(raw.substring(3, 5), 10);
+
+        ZonedDateTime result = ZonedDateTime.now(ZoneId.of("Europe/Kiev"))
+                .withHour(hour)
+                .withMinute(minute);
+
+        if (isYesterday) {
+            return result.minusDays(1);
+        }
+
+        return result;
+    }
+
+    private CompletableFuture<Optional<String>> extractImageURL(URI postURI) {
+        return httpClient.sendAsync(buildPageRequest(postURI), BodyHandlers.ofString(charset()))
+                .thenApply(response -> {
+                    if (response.statusCode() != 200) {
+                        logger.error("Failed to fetch article, status {}", response.statusCode());
+                        return Optional.empty();
+                    }
+                    String body = response.body();
+                    if (body == null || body.isBlank()) {
+                        logger.warn("Cannot parse article response, body is blank");
+                        return Optional.empty();
+                    }
+
+                    Document document = Jsoup.parse(response.body(), Parser.htmlParser());
+                    return extractImageURLFromDocument(document);
+                });
+    }
+
+    private HttpRequest buildPageRequest(URI uri) {
+        return HttpRequest.newBuilder(uri)
+                .GET()
+                .header("Content-Type", "text/html")
+                .timeout(DEFAULT_TIMEOUT)
+                .build();
+    }
+
+    private Optional<String> extractImageURLFromDocument(Document document) {
+        return document.getElementsByTag("img")
+                .stream()
+                .filter(e -> e.hasAttr("src")
+                        && e.hasAttr("alt")
+                        && e.hasAttr("title")
+                        && e.hasClass("img-fluid"))
+                .map(e -> "https://finance.liga.net" + e.attr("src"))
+                .findFirst();
+    }
+}
