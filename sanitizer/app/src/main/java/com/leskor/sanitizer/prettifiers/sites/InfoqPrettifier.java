22143168dcba9af8e65552aa8ebf6b8e40a9aa07
Ruslan Lesko <ruslanlesko@gmail.com>
2022-10-30 12:51:06 +0100

    Add support for code blocks for infoq.com

--- sanitizer/app/src/main/java/com/leskor/sanitizer/prettifiers/sites/InfoqPrettifier.java
+++ sanitizer/app/src/main/java/com/leskor/sanitizer/prettifiers/sites/InfoqPrettifier.java
@@ -1,10 +1,12 @@
 package com.leskor.sanitizer.prettifiers.sites;
 
 import java.util.List;
+import java.util.stream.Stream;
 import com.leskor.sanitizer.entities.Paragraph;
 import com.leskor.sanitizer.entities.Post;
 import com.leskor.sanitizer.prettifiers.Prettifier;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 import org.jsoup.parser.Parser;
 import org.jsoup.safety.Safelist;
 
@@ -14,8 +16,27 @@ public class InfoqPrettifier implements Prettifier {
         return Jsoup.parse(post.html(), Parser.htmlParser())
                 .getElementsByTag("p")
                 .stream()
-                .map(p -> Jsoup.clean(p.html(), Safelist.none()).trim())
-                .map(p -> new Paragraph(p, ""))
+                .flatMap(this::extractParagraphWithCodeElements)
+                .map(this::createParagraph)
                 .toList();
+    }
+
+    private Stream<Element> extractParagraphWithCodeElements(Element p) {
+        Element nextSibling = p.nextElementSibling();
+        Element nextCodeElement = null;
+        if (nextSibling != null && !"p".equals(nextSibling.tagName())) {
+            nextCodeElement = nextSibling.getElementsByTag("pre").first();
+        }
+        if (nextCodeElement == null) return Stream.of(p);
+
+        Element innerCodeElement = nextCodeElement.getElementsByTag("code").first();
+        return Stream.of(p, innerCodeElement == null ? nextCodeElement : innerCodeElement);
+    }
+
+    private Paragraph createParagraph(Element element) {
+        if ("pre".equals(element.tagName()) || "code".equals(element.tagName())) {
+            return new Paragraph(element.html().replaceAll("\\n", "<br>"), "code");
+        }
+        return new Paragraph(Jsoup.clean(element.html(), Safelist.none()).trim(), "");
     }
 }
