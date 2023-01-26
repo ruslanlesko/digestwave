f18fef2741e487157a6971d0a7c032a8bf1499a1
Ruslan Lesko <ruslanlesko@gmail.com>
2023-01-26 12:25:34 +0100

    Add nightly vacuum into database

--- infra/postsdb/002-init.sql
+++ infra/postsdb/002-init.sql
@@ -19,3 +19,5 @@ SELECT cron.schedule('0 */6 * * *', $$WITH latest AS (
         SELECT ROW_NUMBER() OVER (ORDER BY publication_time DESC), publication_time, hash FROM posts
     ) DELETE FROM posts USING latest WHERE latest.hash = posts.hash AND latest.row_number > 1000$$
 );
+
+SELECT cron.schedule('0 4 * * *', 'VACUUM FULL posts');
