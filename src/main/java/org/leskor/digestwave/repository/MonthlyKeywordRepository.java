bb87060dd6d3bd2587aa859b6a3c72e50ba2792d
Ruslan Lesko <ruslanlesko@gmail.com>
2025-04-29 22:31:26 +0200

    Implement top keywords listing wih HTMX UI

--- src/main/java/org/leskor/digestwave/repository/MonthlyKeywordRepository.java
+++ src/main/java/org/leskor/digestwave/repository/MonthlyKeywordRepository.java
@@ -1,13 +1,16 @@
 package org.leskor.digestwave.repository;
 
+import java.util.List;
 import org.leskor.digestwave.model.MonthlyKeyword;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.data.cassandra.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MonthlyKeywordRepository extends CassandraRepository<MonthlyKeyword, String> {
+public interface MonthlyKeywordRepository extends CassandraRepository<MonthlyKeyword, MonthlyKeyword.Key> {
 
     @Query("UPDATE monthlykeyword SET count = count + ?2 WHERE monthyear = ?0 AND keyword = ?1")
     void incrementCount(String monthYear, String keyword, long count);
+
+    List<MonthlyKeyword> findByMonthYearIs(String monthYear);
 }
