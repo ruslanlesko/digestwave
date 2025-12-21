cf6eed751e011fee83c25e5d03fa7c77f84bffba
Ruslan Lesko <ruslanlesko@gmail.com>
2025-12-21 23:30:02 +0100

    Fix Redis config to use application parameters

--- src/main/java/com/leskor/digestwave/config/CacheConfig.java
+++ src/main/java/com/leskor/digestwave/config/CacheConfig.java
@@ -1,22 +1,30 @@
 package com.leskor.digestwave.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class CacheConfig {
     @Bean
-    RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+    RedisConnectionFactory redisConnectionFactory(
+            @Value("${spring.data.redis.host}") String host,
+            @Value("${spring.data.redis.port}") int port) {
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(host, port);
+        factory.afterPropertiesSet();
+        return factory;
     }
 
     @Bean
     RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
         return template;
     }
 }
